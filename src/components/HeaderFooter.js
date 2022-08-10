8ab37d8c8b90696ec6fb132a6759278d987799db
Miguel <miguezaga2@hotmail.com>
2022-08-10 13:57:24 -0500

    style header-footer for mobile

--- src/components/HeaderFooter.js
+++ src/components/HeaderFooter.js
@@ -1,7 +1,9 @@
+import logo from "../assets/logo.svg";
+
 function HeaderFooter() {
   return (
     <div className="header-footer">
-      <img src="" alt="" className="header-footer__img" />
+      <img src={logo} alt="website logo" className="header-footer__img" />
       <button className="header-footer__btn btn1">Free Consultation</button>
     </div>
   );

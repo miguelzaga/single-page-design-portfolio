a922a2433a7cb589457178bd23beef664fd72e31
Miguel <miguezaga2@hotmail.com>
2022-08-11 14:38:25 -0500

    add slide functionality for mobile

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -3,7 +3,6 @@ function Cta() {
     <section className="cta">
       <h2 className="cta__title">Book a call with me</h2>
       <p className="cta__text">
-        {" "}
         I’d love to have a chat to see how I can help you. The best first step
         is for us to discuss your project during a free consultation. Then we
         can move forward from there.
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import arrowLeft from "../assets/icon-arrow-left.svg";
 import arrowRight from "../assets/icon-arrow-right.svg";
 import img1 from "../assets/image-slide-1.jpg";
@@ -7,20 +8,41 @@ import img4 from "../assets/image-slide-4.jpg";
 import img5 from "../assets/image-slide-5.jpg";
 
 function Slider() {
+  var [slide, setSlide] = useState(2);
+
   return (
     <div className="slider">
-      <ul className="slider__main">
-        <img src={img1} alt="" className="slider__img" />
-        <img src={img2} alt="" className="slider__img" />
-        <img src={img3} alt="" className="slider__img" />
-        <img src={img4} alt="" className="slider__img" />
-        <img src={img5} alt="" className="slider__img" />
-      </ul>
+      <figure className="slider__main">
+        <div
+          className="slider__viewport"
+          style={{ marginLeft: `${(slide - 2) * (270 + 16)}px` }}
+        >
+          <img src={img1} alt="" className="slider__img" />
+          <img src={img2} alt="" className="slider__img" />
+          <img src={img3} alt="" className="slider__img" />
+          <img src={img4} alt="" className="slider__img" />
+          <img src={img5} alt="" className="slider__img" />
+        </div>
+      </figure>
       <div className="slider__controls">
-        <button className="slider__btn arrow">
+        <button
+          className="slider__btn arrow"
+          onClick={function moveSliderRight() {
+            if (slide < 4) {
+              setSlide(slide + 1);
+            }
+          }}
+        >
           <img src={arrowLeft} alt="" className="arrow__img" />
         </button>
-        <button className="slider__btn arrow">
+        <button
+          className="slider__btn arrow"
+          onClick={function moveSliderLeft() {
+            if (slide > 0) {
+              setSlide(slide - 1);
+            }
+          }}
+        >
           <img src={arrowRight} alt="" className="arrow__img" />
         </button>
       </div>

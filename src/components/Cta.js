d605f9e525798c606383e22cc46d37a0619180dc
Miguel <miguezaga2@hotmail.com>
2022-08-15 16:58:54 -0500

    style cta for desktop

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,12 +1,14 @@
 function Cta() {
   return (
     <section className="cta">
-      <h2 className="cta__title">Book a call with me</h2>
-      <p className="cta__text">
-        I’d love to have a chat to see how I can help you. The best first step
-        is for us to discuss your project during a free consultation. Then we
-        can move forward from there.
-      </p>
+      <div className="cta__main">
+        <h2 className="cta__title">Book a call with me</h2>
+        <p className="cta__text">
+          I’d love to have a chat to see how I can help you. The best first step
+          is for us to discuss your project during a free consultation. Then we
+          can move forward from there.
+        </p>
+      </div>
       <button className="cta__btn btn2">Free Consultation</button>
     </section>
   );
